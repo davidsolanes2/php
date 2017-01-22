170b1589cd32cd85e3ea05a33be86164270ffea6
davidsolanes2 <davidsolanes2@gmail.com>
2017-01-22 13:40:31 +0100

    version 5.2

--- UF2/Objetos/concesionario.js
+++ UF2/Objetos/concesionario.js
@@ -25,10 +25,6 @@ function Concesionario() {
         return this.pedidos.length;
     };
 
-	this.getpedidos=function (numpedidos) {
-		return this.pedidos(numpedidos);
-    };
-
     this.setNombre = function (nombre) {
 		this.nombre = nombre;
     };
@@ -56,6 +52,8 @@ function Coche() {
     }
 }
 
+
+
 var coche = new Coche();
 coche.nombre="Mustang";
 coche.precio="70.000€";
@@ -71,13 +69,32 @@ var extra = coche.getExtra(numExtra-1);
 
 var posicionPedido = concesionario.addpedidos(coche);
 
-concesionario.addpedidos(coche.nombre="Toyota Verso");
-concesionario.addpedidos(coche.precio="18.000€");
+//num=Math.floor(Math.random()*3);
 
-concesionario.addpedidos(coche.nombre="Peugot 5008");
-concesionario.addpedidos(coche.precio="18.000€");
+var coche1 = new Coche();
+coche1.nombre = "Toyota Verso";
+coche1.precio = "18.000 €";
+coche1.addExtra(extras[(Math.floor(Math.random()*4))]);
+concesionario.addpedidos(coche1);
 
-//concesionario.addpedidos(coche.nombre="Toyota Verso");
+var coche2 = new Coche();
+coche2.nombre = "Peugot 5008";
+coche2.precio = "18.000 €";
+coche2.addExtra(extras[(Math.floor(Math.random()*4))]);
+concesionario.addpedidos(coche2);
+
+var coche3 = new Coche();
+coche3.nombre = "Renault Megane";
+coche3.precio = "17.500 €";
+coche3.addExtra(extras[(Math.floor(Math.random()*4))]);
+concesionario.addpedidos(coche3);
+
+var coche4 = new Coche();
+coche4.nombre = "Camaro";
+coche4.precio = "87.500 €";
+coche4.addExtra(extras[(Math.floor(Math.random()*4))]);
+concesionario.addpedidos(coche4);
+
 
 document.write('<table width="70%" border="1" cellpadding="5" cellspacing="1" bordercolor="#000000" align="center">');
     document.write('<tr bgcolor="#9596ff">');
@@ -102,21 +119,26 @@ document.write('<table width="70%" border="1" cellpadding="5" cellspacing="1" bo
 		document.write('<th>Precio</th>');
 		document.write('<th>Extra</th>');
         document.write('</tr>');
-		document.write('<td align="center">');
-			document.write(posicionPedido);
-		document.write('</td>');
-        document.write('<td align="center">');
-			for(k=0; k<concesionario.pedidos.length; k++) {
-                document.write(concesionario.pedidos[k]);
-			}
-		document.write('</td>');
-        document.write('<td align="right">');
-            document.write(coche.precio);
-        document.write('</td>');
-        document.write('<td>');
-			for(i=0; i<coche.extras.length;i++) {
-                document.write('- '+ coche.extras[i] + '</br>');
-			}
-        document.write('</td>');
+
+		document.write('<tr align="center">');
+
+				for (k = 0; k<concesionario.pedidos.length; k++) {
+					document.write('<tr>');
+                    	document.write('<td>');
+                    		document.write(k+1);
+                    	document.write('</td>');
+                    	document.write('<td>');
+							document.write(concesionario.pedidos[k].nombre);
+						document.write('</td>');
+						document.write("<td align='right'>");
+							document.write(concesionario.pedidos[k].precio);
+						document.write('</td>');
+						document.write('<td>');
+							document.write(concesionario.pedidos[k].extras);
+						document.write('</td>');
+					document.write('</tr>');
+				}
+
+		document.write('</tr>');
 	document.write('</table>');
 document.write('</table> ');
